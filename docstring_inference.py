104a6bdd4018516a0b828ad0165b48a18dc720d3
George Schneeloch <noisecapella@gmail.com>
2014-05-11 18:13:10 -0400

    Changes from last weekend

--- docstring_inference.py
+++ docstring_inference.py
@@ -1,18 +1,31 @@
 from astroid import MANAGER, UseInferenceDefault, inference_tip, YES, InferenceError, nodes
 from astroid.builder import AstroidBuilder
 
+import xml.etree.ElementTree as etree
+from docutils.core import publish_doctree
+
+def register(linter):
+    pass
+
 def infer_from_docstring(node, context=None):
     
-    def infer_first(node):
-        try:
-            value = node.infer().next()
-            if value is YES:
-                raise UseInferenceDefault()
-            else:
-                return value
-        except StopIteration:
-            raise InferenceError()
+    docstring = node.getattr('__doc__')[0].value
+    if docstring is None:
+        return
+    
+    doctree = etree.fromstring(publish_doctree(docstring).asdom().toxml())
+    field_lists = doctree.findall(".//field_list")
+    fields = [f for field_list in field_lists
+              for f in field_list.findall('field')]
+    if fields:
+        for field in fields:
+            raise Exception(publish_doctree(docstring).asdom().toxml())
+            field_name = field.findall("field_name")[0].text
+            field_body = field.findall("field_body")[0].findall("paragraph")[0].text
+            print "XXX: %s, YYY: %s" % (field_name, field_body)
+    # TODO: parse each field in fields to types, then
+    # return iter([node]) where node is like the input but
+    # altered somehow to provide inference information
 
-
-MANAGER.register_transform(nodes.CallFunc, inference_tip(infer_from_docstring))
+MANAGER.register_transform(nodes.Function, infer_from_docstring)
 
