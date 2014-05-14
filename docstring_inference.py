d13bc77b7bc6d3ecfd896f1b625db14f50911fcd
George Schneeloch <noisecapella@gmail.com>
2014-05-14 07:43:20 -0400

    Parse rtype as class for now

--- docstring_inference.py
+++ docstring_inference.py
@@ -7,25 +7,49 @@ from docutils.core import publish_doctree
 def register(linter):
     pass
 
-def infer_from_docstring(node, context=None):
-    
-    docstring = node.getattr('__doc__')[0].value
-    if docstring is None:
-        return
-    
-    doctree = etree.fromstring(publish_doctree(docstring).asdom().toxml())
-    field_lists = doctree.findall(".//field_list")
-    fields = [f for field_list in field_lists
-              for f in field_list.findall('field')]
-    if fields:
-        for field in fields:
-            raise Exception(publish_doctree(docstring).asdom().toxml())
-            field_name = field.findall("field_name")[0].text
-            field_body = field.findall("field_body")[0].findall("paragraph")[0].text
-            print "XXX: %s, YYY: %s" % (field_name, field_body)
-    # TODO: parse each field in fields to types, then
-    # return iter([node]) where node is like the input but
-    # altered somehow to provide inference information
+def parse_node(text):
+    if text == "None":
+        clazz = "NoneType"
+    else:
+        clazz = text
 
-MANAGER.register_transform(nodes.Function, infer_from_docstring)
+    return nodes.Class(clazz, 'docstring').instanciate_class()
+
+def infer_from_docstring(node, context=None):
+    for infer in node.func.infer(context):
+        
+        docstring = infer.doc
+        if docstring is None:
+            return
+        
+    
+        doctree = etree.fromstring(publish_doctree(docstring).asdom().toxml())
+        field_lists = doctree.findall(".//field_list")
+        fields = [f for field_list in field_lists
+                  for f in field_list.findall('field')]
+
+        if fields:
+            for field in fields:
+                field_name = field.findall("field_name")[0].text
+                field_body = field.findall("field_body")[0].findall("paragraph")[0].text
+            
+                if field_name.startswith("rtype"):
+                    name = nodes.Name()
+                    name.name = field_body
+                    ret_node = parse_node(field_body)
+                    return iter([ret_node])
+        # found nothing
+        
+def infer_enum(node, context=None):
+    class_node = nodes.Class("ABC", 'docstring')
+    return iter([class_node.instanciate_class()])
+
+
+def wrap_exception(node, context=None):
+    try:
+        return infer_from_docstring(node, context)
+    except InferenceError, UseInferenceDefault:
+        return None
+
+MANAGER.register_transform(nodes.CallFunc, inference_tip(infer_from_docstring))
 
