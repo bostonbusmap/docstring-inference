dfb387906d124f1878396204bf629305715ce4b8
George Schneeloch <bostonbusmap@gmail.com>
2014-05-30 12:22:53 -0400

    Show how declared type overrides inferred type

--- test/test_class.py
+++ test/test_class.py
@@ -8,7 +8,7 @@ def f(x, y):
     :rtype: c.f.D
 """
     # TODO: A should collide with c.f.D()
-    return c.f.D()
+    return A()
 
 # TODO: the string arguments here should collide with the 'int' in the docstring
 # and produce an error.
